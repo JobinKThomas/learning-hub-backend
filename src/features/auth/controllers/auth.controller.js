4c5f2bb735c2a76974515094d38e76d4a3185274
JobinK-Thomas <jobinthomas@ikm.gov.in>
2026-07-03 16:10:53 +0530

    feat(auth): add repository, token service, dto, validators and routes

---
+++ src/features/auth/controllers/auth.controller.js
@@ -0,0 +1,9 @@
+export const register = async (req, res) => {};
+
+export const login = async (req, res) => {};
+
+export const logout = async (req, res) => {};
+
+export const refresh = async (req, res) => {};
+
+export const me = async (req, res) => {};
---
+++ src/features/auth/validators/auth.validator.js
@@ -0,0 +1,26 @@
+import { body } from "express-validator";
+
+export const registerValidator = [
+  body("firstName")
+    .trim()
+    .notEmpty()
+    .withMessage("First name is required"),
+
+  body("email")
+    .isEmail()
+    .withMessage("Valid email is required"),
+
+  body("password")
+    .isLength({ min: 8 })
+    .withMessage("Password must be at least 8 characters"),
+];
+
+export const loginValidator = [
+  body("email")
+    .isEmail()
+    .withMessage("Valid email is required"),
+
+  body("password")
+    .notEmpty()
+    .withMessage("Password is required"),
+];
